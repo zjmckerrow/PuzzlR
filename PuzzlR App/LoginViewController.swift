19184eb2808758c3f9285b4edc5c5f8a807a01fe
Zach McKerrow <zmckerrow@yahoo.com>
2019-06-24 12:35:13 -0500

    yeet

--- PuzzlR App/LoginViewController.swift
+++ PuzzlR App/LoginViewController.swift
@@ -1,47 +1,48 @@
 //
 //  LoginViewController.swift
-//  PuzzlR App
+//  PuzzlR_App
 //
-//  Created by Zach McKerrow on 6/8/19.
+//  Created by Zach McKerrow on 6/21/19.
 //  Copyright © 2019 PuzzlR. All rights reserved.
 //
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var usernameTextField: UITextField!
-    
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-    }
-    
-    @IBAction func loginButtonTapped(_ sender: Any) {
-        
-        ToMainInterface()
         
     }
     
-    private func ToMainInterface() {
+    @IBAction func loginAction(_ sender: Any) {
         
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        
-        guard let mainNavigationViewController = mainStoryboard.instantiateViewController(withIdentifier: "MainNavigationController") as? MainNavigationController else {
-            return
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) {
+            (user, error) in
+            if error == nil {
+                
+                self.performSegue(withIdentifier: "loginToHome", sender: self)
+                
+            }
+            else {
+                
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+            }
+            
         }
-        
-        print(mainNavigationViewController.topViewController)
-        print(mainNavigationViewController.viewControllers)
-        
-        if let mainVC = mainNavigationViewController.topViewController as? ViewController {
-            mainVC.username = usernameTextField.text
-            mainVC.password = passwordTextField.text
-        }
-        
-        present(mainNavigationViewController, animated: true, completion: nil)
         
     }
     
